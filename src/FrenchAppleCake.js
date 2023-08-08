53167118871a5db0607f86daa1bd0c7d5134f652
nezhlya <eminejla@gmail.com>
2023-08-08 15:52:47 +0200

    grid added

--- src/FrenchAppleCake.js
+++ src/FrenchAppleCake.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./FrenchAppleCake.css";
-import Title from "./Title.js";
 import Body from "./Body.js";
 
 export default function FrenchAppleCake() {
   return (
     <div className="FrenchAppleCake">
       <div className="container-fluid">
-        <Title />
+      
         <Body />
       </div>
     </div>
--- src/test.js
+++ src/test.js
@@ -1,13 +1,20 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./Body.css";
-import cake from "./French-Apple-Cake.jpg";
-import cakePowder from "./Cake-powder.jpg";
+import React from 'react';
+import "./Body.css"
 
-export default function Body() {
-  return (
+
+export default function test(){
+return (
+  <div>
     <div className="grid">
-      <div className="left"></div>
+      <a
+        className="magazine"
+        href="https://res.cloudinary.com/jduquain/q_auto,f_auto/v1540686705/magazine-2_kngpgy.jpg"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Inspiration
+      </a>
+      <div className="background"></div>
 
       <h1 className="main-heading">
         French<span>Apple</span>Cake
@@ -18,6 +25,11 @@ export default function Body() {
         Fusion of Flavors and Elegance
       </p>
 
+      <img
+        src="https://res.cloudinary.com/jduquain/q_auto,f_auto/v1540686287/squiggle-2_bwd6id.png"
+        alt="squiggle"
+        class="squiggle"
+      />
 
       <div className="para-wrap--one">
         <p className="para-one">
@@ -44,13 +56,47 @@ export default function Body() {
           </p>
         </p>
         <p className="para-two">
-          
+          Sunt ratione quo earum voluptatem sint asperiores, optio cupiditate!
+          Eius ratione sint distinctio ad obcaecati, magni dolor illo, vero non,
+          corporis, blanditiis excepturi velit repudiandae ab quas? Officia
+          doloremque repudiandae, minima veniam quam nisi nesciunt iure fugiat
+          animi, temporibus voluptatem molestiae quae aliquid unde, aspernatur
+          culpa quas a. Id,&nbsp;iure!
         </p>
+        <p className="para-three">
+            The French
+            <a href="https://en.wikipedia.org/wiki/Apple_cake">Apple Cake</a>,
+            also known as "Gateau aux Pommes" in French, is a classic and
+            beloved dessert in French cuisine. It is a simple yet elegant cake
+            that beautifully showcases the natural sweetness and flavor of
+            apples. This delightful treat is commonly served in French
+            households, patisseries, and cafes. The French Apple Cake typically
+            consists of a tender and moist batter infused with small chunks or
+            slices of fresh apples. The apples are often mixed with a touch of
+            cinnamon or vanilla, adding a warm and aromatic note to the cake.
+            The recipe for French Apple Cake is quite versatile, allowing for
+            various adaptations and personal touches. Some versions use a
+            buttery batter similar to that of a pound cake, while others
+            incorporate a lighter texture with the addition of yogurt or crème
+            fraîche. One unique aspect of the French Apple Cake is its rustic
+            appearance. It is often baked in a round or rectangular cake pan,
+            without the use of intricate decorations or frosting. The focus is
+            on the simplicity of the ingredients and the natural beauty of the
+            baked apples peeking through the golden cake.
+          </p>
       </div>
 
-      <img src={cakePowder} alt="cake" className="cake" />
+      <img
+        src="https://res.cloudinary.com/jduquain/q_auto,f_auto/v1540686288/cake-2_ar9aud.jpg"
+        alt="cake"
+        class="cake"
+      />
 
-      <img src={cake} alt="powdered-sugar" className="cupcake" />
+      <img
+        src="https://res.cloudinary.com/jduquain/q_auto,f_auto/v1540686287/cupcake-2_yfoipb.jpg"
+        alt="cupcakes"
+        class="cupcake"
+      />
 
       <div className="para-wrap--two">
         <p className="para-four">
@@ -78,12 +124,10 @@ export default function Body() {
           error. Voluptate non fugit molesti, consectet sapiente
           soluta&nbsp;illum.
         </p>
-        <img
-          src="https://res.cloudinary.com/jduquain/q_auto,f_auto/v1540686287/squiggle-2_bwd6id.png"
-          alt="squiggle"
-          class="squiggle"
-        />
+        <i className="fa fa-arrow-alt-circle-right"></i>
       </div>
     </div>
-  );
-}
+
+    <p className="fixed">In the Know</p>
+  </div>
+);}
