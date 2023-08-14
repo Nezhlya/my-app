d2bbfbfab9004cc5c3a0a33e92b3abbaa79077f3
nezhlya <eminejla@gmail.com>
2023-08-14 17:10:51 +0200

    comment box added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
-import FrenchAppleCake from "./FrenchAppleCake.js";
+import Dumpling from "./Dumpling.js";
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <FrenchAppleCake />
+        <Dumpling />
       </header>
       <footer className="App-footer">
         <img src={logo} className="App-logo" alt="logo" />Coded by Nezhlya Emin 👩‍💻Open-source code in Github
--- src/Dumpling.js
+++ src/Dumpling.js
@@ -1,20 +1,23 @@
 import React from "react";
-import "./FrenchAppleCake.css";
+import "./Dumpling.css";
 import Body from "./Body.js";
+import CommentBox from "./CommentBox.js";
 
-export default function FrenchAppleCake() {
+export default function Dumpling() {
   return (
-    <div className="FrenchAppleCake">
-      <div className="container-fluid">
+   
+      <div>
         <div className="page1"></div>
         <div className="page2wrapper">
           <div className="page2">
             <div className="content">
               < Body />
+              <CommentBox />
             </div>
           </div>
         </div>
-      </div>
     </div>
   );
 }
+
+
