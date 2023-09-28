b17d30304722d0c7684e74a9aafc3c2f8712c9c1
Nezhlya <122394560+Nezhlya@users.noreply.github.com>
2023-09-28 02:03:54 +0200

    wrapper aded

--- src/Body.js
+++ src/Body.js
@@ -5,9 +5,11 @@ import RecipeCard from "./RecipeCard";
 
 export default function Body() {
   return (
-    <div>
-      <h1>Dumplings</h1>
-      <p className="intro">
+    <div className="container">
+      <div className="intro"><h1>Dumplings</h1>
+      <p>
+      Are you ready to embark on a culinary journey that will take your taste buds on a whirlwind adventure? You've come to the right place! 
+      I am passionate about sharing the secrets behind crafting these delectable parcels of joy that have captured the hearts of food lovers worldwide. 
         Chinese dumplings, or jiaozi, are one of the most historic foods in
         existence. Much of the food we eat today has only been around for a few
         hundred years, but the Chinese have made jiaozi for over 2000 years.
@@ -18,7 +20,10 @@ export default function Body() {
           Learn more on
           <a href="https://en.wikipedia.org/wiki/Dumpling"> Wikipedia.</a>
         </p>
-      </p>
+      </p></div>
+      <div className="wrapper">
+        <p>Dumpling wrappers, also known as dumpling skins or dumpling wrappers, are a fundamental component of various types of dumplings found in many Asian cuisines and beyond. </p>
+      </div>
     
       <div className="recipeCards ">
         <div className="col">
